684a50715e19efd52d4ab47ebcdc968ffedbfc45
Rochelle Younger <regnuoy321@gmail.com>
2022-01-09 18:07:39 -0500

    questions are displayed, answers validate, and right/wrong logged correctly upon validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// quiz info
 var quizObjects = [
     {
         question: "A spider has __ legs.",
@@ -66,3 +67,117 @@ var quizObjects = [
         correct: "a) arachnophobia",
     },
 ];
+
+
+// grab elements from page to manipulate
+
+var firstSection = document.getElementById("quiz-intro");
+var quizSection = document.getElementById("quiz-questions");
+var startButton =  document.getElementById("play");
+var timerEl = document.getElementById("timer");
+var answerSection = document.getElementById("answer-btns");
+var judgementDivEl = document.getElementById("judgement");
+var judgementalText = document.getElementById("judgemental-text");
+
+// hides cards
+var hide = function() {
+    firstSection.setAttribute("hidden", true)
+};
+
+// waiting until after answer is given to display judgement
+var hideJudgement = function() {
+    judgementDivEl.setAttribute("hidden", true)
+};
+
+var currentInterval = 0;
+var time = 60;
+var displayedQuestion = 0;
+
+
+// starts quiz game
+var play = function() {
+    hide();
+    quizSection.removeAttribute("hidden");
+
+    askQuestion();
+
+    currentInterval = setInterval(countdown, 1000);
+
+    // time();
+
+};
+
+var countdown = function() {
+    time--;
+    // time();
+    if(time < 1) {
+        stop();
+    }
+};
+
+// displays the countdown 
+var timer = function() {
+    timerEl.textContent = time;
+};
+
+var askQuestion = function() {
+    var questionInfo = quizObjects[displayedQuestion];
+    var answers = questionInfo.answers;
+
+    var questionTextEl = document.getElementById("quiz-question");
+    questionTextEl.textContent = questionInfo.question;
+
+    for (var i=0; i < answers.length; i++) {
+        var answer = answers[i];
+        var answerBtnEl = document.getElementById("answer-btn" + i)
+        answerBtnEl.textContent = answer;
+    }
+};
+
+var correct = function(answerBtnEl) {
+    if (answerBtnEl.textContent === quizObjects[displayedQuestion].correct) {
+        return true;
+    } else {
+        return false
+    }
+};
+
+var answerCheck = function(event) {
+    var clickedAnswer = event.target;
+    judgementDivEl.removeAttribute("hidden");
+    if (correct(clickedAnswer)) {
+        console.log("right")
+        judgementalText = "Satisfactory";
+        setTimeout(hideJudgement, 1000);
+    } else {
+        console.log("wrong")
+        judgementalText = "Failure :|";
+        setTimeout(hideJudgement, 1000);
+        if(time >= 6) {
+            time = time - 6;
+        } else {
+            time =0;
+            stop();
+        }
+    }
+
+    displayedQuestion++;
+    if(displayedQuestion < quizObjects.length) {
+        askQuestion();
+    } else {
+        stop();
+    }
+}
+
+var stop = function() {
+    clearInterval(currentInterval);
+    hide();
+    console.log("gameOver")
+}
+
+
+// event listeners on dom objects
+// start the game
+startButton.addEventListener("click", play);
+// answering
+answerSection.addEventListener("click", answerCheck)
