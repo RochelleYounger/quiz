87305156bea39d4754bd18e48f4430e4dd14bfbb
Rochelle Younger <regnuoy321@gmail.com>
2022-01-09 16:07:14 -0500

    added basic HTML and questions array in script.js

---
+++ README.md
@@ -0,0 +1,9 @@
+# quiz
+
+## Purpose
+
+To create a timed quiz that is dynamically update. Users can store scores and high scores in localStorage which will be displayed at the end of the quiz. 
+
+
+
+## Link to Deployed Application
---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+var quizObjects = [
+    {
+        question: "A spider has __ legs.",
+        img: "",
+        answers: ["a) 1000", "b) 6", "c) 8", "d) 10"],
+        correct: "c) 8",
+    },
+    {
+        question: "A spider has __ eyes.",
+        img: "",
+        answers: ["a) 4", "b) 2", "c) 7", "d) 8"],
+        correct: "d) 8",
+    },
+    {
+        question: "Spiders have two main body parts: the _________ and the _________.",
+        img: "",
+        answers: ["a) cephalothorax, abdomen", "b) head, cephalothorax", "c) pronotoum, abdomen", "d) mandibles, sting"],
+        correct: "a) cephalothorax, abdomen",
+    },
+    {
+        question: "Except for one small family ___________, all spiders have venom which they inject into prey through their fangs to digest them.",
+        img: "",
+        answers: ["a) Agelenidae", "b) Theraphosidae", "c) Salticidae", "d) Uloboridae"],
+        correct: "d) Uloboridae",
+    },
+    {
+        question: "Jumping spiders don't build webs.",
+        img: "",
+        answers: ["a) true", "b) false", "c) I don't know.", "d) yes and no"],
+        correct: "a) true",
+    },
+    {
+        question: "Lucas the Spider is a jumping spider.",
+        img: "",
+        answers: ["a) true", "b) false", "c) I don't know.", "d) yes and no"],
+        correct: "a) true",
+    },
+    {
+        question: "There is a species of herbivore(ish) jumping spiders that are found in Central America. It's name is ____________",
+        img: "",
+        answers: ["a) Ero furcata", "b) Bagheera kiplingi", "c) Phidippus pius", "d) Plexippus paykulli"],
+        correct: "",
+    },
+    {
+        question: "The scientific name for spiders is _________",
+        img: "",
+        answers: ["a) Spider", "b) Anarae", "c) Araignée", "d) Araneae"],
+        correct: "d) Aranae",
+    },
+    {
+        question: "The full scientific classification for spiders is: Kingdom-________, Phylum-_________, Subphylum-_________, Class-__________, Order-__________",
+        img: "",
+        answers: ["a) Arthropoda, Chelicerata, Animalia, Arachnida, Araneae", "b) Animalia, Arthropoda, Chelicerata, Arachnida, Araneae", "c) Animalia, Chelicerata, Arthropoda, Arachnida, Araneae", "d) "],
+        correct: "b) Animalia, Arthropoda, Chelicerata, Arachnida, Araneae",
+    },
+    {
+        question: "Most spiders are harmful to humans.",
+        img: "",
+        answers: ["a) true", "b) false", "c) I don't know.", "d) yes and no"],
+        correct: "b) false",
+    },
+    {
+        question: "Extreme or irrational fear of spiders is called _____________",
+        img: "",
+        answers: ["a) arachnophobia", "b) arachniphobia", "c) arachnephobia", "d) arachnaphobia"],
+        correct: "a) arachnophobia",
+    },
+];
